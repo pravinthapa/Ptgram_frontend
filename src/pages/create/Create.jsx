8e529c2898298d3234a6bbd0bc45c9dc0c85c2ff
pravinthapa <prabinthapaparbin33@gmail.com>
2025-03-01 17:30:54 +0545

    create

---
+++ src/pages/create/Create.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TbPhotoVideo } from "react-icons/tb";
+export default function Create() {
+  return (
+    <div>
+      <h1> Create new post</h1>
+
+      <TbPhotoVideo />
+      <button>select from computer</button>
+    </div>
+  );
+}
